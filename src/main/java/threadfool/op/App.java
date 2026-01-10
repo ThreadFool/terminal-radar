50ecad0f27c011a97386948a3905458c1bfb4b59
I583075 <rafal.szygula@sap.com>
2026-01-11 00:17:55 +0100

    Over 10 aircrafts handling

--- src/main/java/threadfool/op/App.java
+++ src/main/java/threadfool/op/App.java
@@ -1,5 +1,9 @@
 package threadfool.op;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -8,6 +12,34 @@ public class App
 {
 	public static void main(String[] args)
 	{
+		String url = "jdbc:h2:./data/adsbdb";
+		String user = "sa";
+		String pass = "";
+
+		try (Connection conn = DriverManager.getConnection(url, user, pass);
+			 Statement stmt = conn.createStatement()) {
+
+			stmt.execute("""
+                CREATE TABLE IF NOT EXISTS flight (
+                    id IDENTITY PRIMARY KEY,
+                    icao24 VARCHAR(6),
+                    callsign VARCHAR(16),
+                    ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                )
+            """);
+
+			stmt.execute("""
+                INSERT INTO flight (icao24, callsign)
+                VALUES ('48a123', 'LOT123')
+            """);
+
+			System.out.println("Worked");
+		}
+		catch (SQLException e)
+		{
+			throw new RuntimeException(e);
+		}
+
 		Configuration configuration = new Configuration();
 		ConcurrentLinkedQueue<Integer> freeIds = new ConcurrentLinkedQueue<>();
 		Map<String, AircraftState> aircrafts = new ConcurrentHashMap<>();
